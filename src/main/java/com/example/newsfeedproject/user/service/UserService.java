41f14baede5595ad8ac17bd3af94264722dbe21c
Minsang <minsang1233@gmail.com>
2024-11-20 20:00:22 +0900

    feat: profile exceptional 처리

--- src/main/java/com/example/newsfeedproject/user/service/UserService.java
+++ src/main/java/com/example/newsfeedproject/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.newsfeedproject.user.service;
 
 import com.example.newsfeedproject.common.config.PasswordEncoder;
+import com.example.newsfeedproject.common.util.UtilValidation;
 import com.example.newsfeedproject.user.dto.Signup.SignUpResponseDto;
 import com.example.newsfeedproject.user.entity.User;
 import com.example.newsfeedproject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
@@ -19,7 +22,12 @@ public class UserService {
 
     public SignUpResponseDto signUp(String name, String email, String profileImageUrl, String password) {
 
-        User user = new User(name, email, profileImageUrl, passwordEncoder.encode(password));
+//        // 비밀번호 형식
+//        if (!UtilValidation.isValidPasswordFormat(password)) {
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "비밀번호는 최소 8자, 대소문자 포함한 영문, 숫자, 특수문자를 포함해야합니다.");
+//        }
+
+        User user = new User(name, email, profileImageUrl, passwordEncoder.encode(password)); //password에 대한 유효성 검사 진행 후 문제 없으면 저장
 
         User savedUser = userRepository.save(user);
 
