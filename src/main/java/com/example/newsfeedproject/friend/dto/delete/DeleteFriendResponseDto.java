2115ea58d2e85c3934511ec91e495fc0320840c3
jhsong <thdwjdgkr123@gmail.com>
2024-11-21 19:33:52 +0900

    friend 기능 전체적인 예외 버그 수정 친구 게시글 목록을 가져올때 페이지로 가져오도록 구현(페이저블 사용x 리스트 사용) 친구 게시글 목록 조회 테스트코드 에러발생 하지만 포스트맨 실행시 정상실행

--- src/main/java/com/example/newsfeedproject/friend/dto/delete/DeleteFriendResponseDto.java
+++ src/main/java/com/example/newsfeedproject/friend/dto/delete/DeleteFriendResponseDto.java
@@ -1,5 +1,8 @@
 package com.example.newsfeedproject.friend.dto.delete;
 
+import lombok.Getter;
+
+@Getter
 public class DeleteFriendResponseDto {
     private boolean success;
     public DeleteFriendResponseDto(boolean success) {
--- src/main/java/com/example/newsfeedproject/friend/dto/request/RequestFriendResponseDto.java
+++ src/main/java/com/example/newsfeedproject/friend/dto/request/RequestFriendResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.newsfeedproject.friend.dto.request;
 
+import lombok.Getter;
 
+@Getter
 public class RequestFriendResponseDto {
     private boolean success;
     public RequestFriendResponseDto(boolean success) {
