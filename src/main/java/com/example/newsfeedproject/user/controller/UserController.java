17d0034c580f77619994f279bcd33cb6ca2f67e9
kmj-23 <kimmj1304@gmail.com>
2024-11-22 12:44:25 +0900

    feature/delete-user

--- src/main/java/com/example/newsfeedproject/user/controller/UserController.java
+++ src/main/java/com/example/newsfeedproject/user/controller/UserController.java
@@ -3,15 +3,14 @@ package com.example.newsfeedproject.user.controller;
 import com.example.newsfeedproject.user.dto.Login.LoginRequestDto;
 import com.example.newsfeedproject.user.dto.Signup.SignUpRequestDto;
 import com.example.newsfeedproject.user.dto.Signup.SignUpResponseDto;
-import com.example.newsfeedproject.user.entity.User;
 import com.example.newsfeedproject.user.service.UserService;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/users")
@@ -48,6 +47,23 @@ public class UserController {
         }
         return ResponseEntity.ok("로그아웃 성공");
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id, @RequestBody String password, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session == null) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "로그인해주세요.");
+        }
+        Long userId = (Long) session.getAttribute("userId");
+        if (!userId.equals(id)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("계정 삭제를 할 수 없습니다.");
+
+        }
+        userService.deleteUser(id, password, request);
+        session.invalidate();
+        return ResponseEntity.ok().body("정상적으로 삭제되었습니다.");
+    }
+
 }
 
 
